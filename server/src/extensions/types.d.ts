7d53f6a3757a475240d579ae5e50c42399e5bd6d
Grzegorz Maniak <git@grzegorz.ie>
2023-10-21 02:59:47 +0100

    Added better documentation

--- server/src/extensions/types.d.ts
+++ server/src/extensions/types.d.ts
@@ -6,17 +6,89 @@ import { DATAResponseCode, IMailFrom, IVRFYResponse, RCPTTOResponseCode, VRFYRes
 import { IAddress } from '../email/types';
 
 
-// -- PARSER TYPES
+
+/**
+ * @name ICustomParserReturnType
+ * Return types that the custom command paramater parser can return
+ * 
+ * @type {string} - A string that cannot contain spaces Eg, 'X=HelloWorld' and dosen't need to be surrounded by quotes
+ * @type {phrase} - A string that can contain spaces AND must be surrounded by quotes Eg, 'X="Hello World"'
+ * @type {number} - 'X=100000000'
+ * @type {boolean} - 'X=true' or 'X=false'
+ * @type {none} - Nothing, just the param name Eg, 'X'
+ */
 export type ICustomParserReturnType = 'string' | 'phrase' | 'number' | 'boolean' | 'none';
+
+
+
+/**
+ * @name ICustomParserNeeds
+ * If a paramater is required or optional for the paramater parser
+ * 
+ * @note Optional paramaters can be null and may impact the performance
+ * of the parser if used heavily.
+ * 
+ * @type {REQUIRED} - The paramater is required
+ * @type {OPTIONAL} - The paramater is optional (potentiall to be null) 
+ */
 export type ICustomParserNeeds = 'REQUIRED' | 'OPTIONAL';
+
+
+
+/**
+ * @name ICommandParserOption
+ * A string that contains the return type and if the paramater is required or optional
+ * 
+ * @example 
+ * 'string:REQUIRED' // - A string that is required
+ * 'string:OPTIONAL' // - A string that is optional
+ * 'phrase:REQUIRED' // - A phrase that is required
+ * 'phrase:OPTIONAL' // - A phrase that is optional
+ * 'number:REQUIRED' // - A number that is required
+ * 'number:OPTIONAL' // - A number that is optional
+ * 'boolean:REQUIRED' // - A boolean that is required
+ * 'boolean:OPTIONAL' // - A boolean that is optional
+ * 'none:REQUIRED' // - A paramater that is required
+ * 'none:OPTIONAL' // - A paramater that is optional
+ */
 export type ICommandParserOption = `${ICustomParserReturnType}:${ICustomParserNeeds}`;
+
+
+
+/**
+ * @name ICustomParser
+ * An object passed into the custom command parser that contains the paramaters
+ * that the parser should parse
+ * 
+ * @example
+ * const paramaters = { 'X': 'string:REQUIRED' };
+ * 
+ * const parsed = parse_command('CUSTOM', 'CUSTOM: X=HelloWorld', paramaters);
+ * // parsed => new Map<string, { need: 'REQUIRED', type: 'string', raw: 'X=HelloWorld', data: 'HelloWorld' }>
+ */
 export interface ICustomParser { [data: string]: ICommandParserOption }
+
+
+
+/**
+ * @name IParsedParser
+ * Represents a Map of parsed parameters returned by the custom command parser.
+ *
+ * @property {string} key - The parameter name.
+ * @property {ICustomParserNeeds} need - Indicates whether the parameter is required or optional.
+ * @property {ICustomParserReturnType} type - Specifies the type of the parameter.
+ * @property {string} raw - The raw parameter string.
+ * @property {string | number | boolean} data - The parsed parameter data.
+ */
+
 export type IParsedParser = Map<string, {
     need: ICustomParserNeeds,
     type: ICustomParserReturnType,
     raw: string,
     data: string | number | boolean,
 }>;
+
+
 
 export type ICustomCommandDataCallback = (data: ICustomCommandData) => void | number;
 export interface ICustomCommandData {
@@ -39,7 +111,6 @@ export interface ICustomCommandData {
         parser_time: number,
     }
 }
-
 
 
 export type IExtensionDataCallback = (data: IExtensionData) => void | number;
