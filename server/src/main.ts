e32339327eb6e2dd32e43176935bea37b1339060
Grzegorz Maniak <git@grzegorz.ie>
2023-11-02 19:07:43 +0000

    Fiixed path

--- server/src/main.ts
+++ server/src/main.ts
@@ -31,7 +31,9 @@ export const _runtime: 'BUN' | 'NODE' = (process.env.RUNTIME as 'BUN' | 'NODE' |
 log('INFO', 'Main', 'main', 'Starting server...');
 
 // -- Get the config
-const abs_config_path = `${import.meta.dir}/../basic_config.json`;
+const abs_config_path = _runtime === 'BUN' ?
+    `${import.meta.dir}/../basic_config.json` :
+    `${__dirname}/../basic_config.json`;
 const config = Configuration.get_instance(abs_config_path);
 
 
